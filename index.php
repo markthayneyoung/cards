a1a417fecd7c8c172dab2714ae5215a76aa0764d
Mark Young <10628871@uvu.edu>
2019-02-10 17:02:47 -0700

    PHP Complete

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="drawtwo.php" method="get">
+        <input autofocus type="submit" value="Draw 2 Cards">
+    </form>
+</body>
+</html>
